0e52b2afb8b8eb1555a02815068a94e48285735c
JolloDede <jollodede@gmail.com>
2019-03-13 12:31:35 +0100

    sql script für eisen hower

--- datastruk.sql
+++ datastruk.sql
@@ -1,6 +1,12 @@
+CREATE DATABASE IF NOT EXISTS eh;
+USE eh;
+
 CREATE TABLE eisenhower(
   id int AUTO_INCREMENT not null,
-  user varchar(20);
+  user varchar(20),
   etime time,
+  link varchar(25),
   discription varchar(200),
+  dificulty int,
+  PRIMARY key(id)
 );
--- user.sql
+++ user.sql
@@ -1,4 +1,4 @@
-CREATE user thebauzz identified by "root";
-CREATE user JolloDede indentified by "root";
+CREATE user 'thebauzz'@'localhost' identified by "root";
+CREATE user 'JolloDede'@'localhost' identified by "root";
 
-Grant all on *.* to thebauzz, JolloDede;
+Grant all on *.* to 'thebauzz'@'localhost', 'JolloDede'@'localhost';
