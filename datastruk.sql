c7a279e852317d363494c99e302537ff8bc3644a
JolloDede <jollodede@gmail.com>
2019-03-13 11:38:24 +0100

    anfang der DB

---
+++ datastruk.sql
@@ -0,0 +1,6 @@
+CREATE TABLE eisenhower(
+  id int AUTO_INCREMENT not null,
+  user varchar(20);
+  etime time,
+  discription varchar(200),
+);
---
+++ user.sql
@@ -0,0 +1,4 @@
+CREATE user thebauzz identified by "root";
+CREATE user JolloDede indentified by "root";
+
+Grant all on *.* to thebauzz, JolloDede;
