0b676944237804b00cc42f87a51e49f12c5ada02
JolloDede <jollodede@gmail.com>
2019-03-19 15:49:12 +0100

    progress - v190319-15.49

--- php/data.php
+++ php/data.php
@@ -1,18 +1,25 @@
 <?php
+  session_start();
   $user = $_SESSION["user"];
-  $pw = $_SESSION["pw"]
+  $pw = $_SESSION["pw"];
   if (isset($_POST["datetime-local"]) && isset($_POST["link"]) && isset($_POST["description"]) && isset($_POST["importance"])) {
-    $conn = mysqli_connect('localhost', $uname, $pw, 'eh');
+    $date = $_POST["datetime-local"];
+    $link = $_POST["link"];
+    $desc = $_POST["description"];
+    $imp = $_POST["importance"];
+
+    $conn = mysqli_connect('localhost', $user, $pw, 'eh');
     mysqli_set_charset($conn, "utf8");
     if (!$conn) {
       die("Connection failed");
     }else{
-      $sql = "INSER INTO eisenhower(user, etime, link, description, importance)
-      VALUES ($user, $_POST['date'], $_POST['link'], $_POST['description'], $_POST['importance'])";
+      $sql = "INSERT INTO eisenhower(id, user, etime, link, description, importance)
+      VALUES ('', $user, $date, $link, $desc, $imp)";
       if (mysqli_query($conn, $sql)){
         echo "success";
       }else {
         echo "fail";
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
       }
     }
   }
