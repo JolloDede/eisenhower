21cf4ea73d0afed58987b9a1fd6bd6a6f26fbc4f
marcel <marcel@gertsch.be>
2019-03-19 15:53:55 +0100

    progress - v190319-15.53

--- pages/eisenhower.php
+++ pages/eisenhower.php
@@ -123,8 +123,8 @@
      <div id="newentry--modal" class="">
        <form action="../php/data.php" method="post">
          <fieldset>
-           <input style="font-size: inherit;" name="datetime-local" type="datetime-local" id="datetime-local" placeholder="&nbsp;" required>
-           <label class="form--label" for="datetime-local">Due Date</label><br>
+           <input style="font-size: inherit;" name="date" type="date" id="date" placeholder="&nbsp;" required>
+           <label class="form--label" for="date">Due Date</label><br>
 
            <input name="link" type="text" id="link" placeholder="&nbsp;" required>
            <label class="form--label" for="link">Link</label><br>
