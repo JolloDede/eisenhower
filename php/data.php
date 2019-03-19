941d06afa15605b10b6e397314cef9e19e2bb137
marcel <marcel@gertsch.be>
2019-03-19 16:07:45 +0100

    progress - v190319-16.07

--- php/data.php
+++ php/data.php
@@ -2,22 +2,26 @@
   session_start();
   $user = $_SESSION["user"];
   $pw = $_SESSION["pw"];
-  if (isset($_POST["datetime-local"]) && isset($_POST["link"]) && isset($_POST["description"]) && isset($_POST["importance"])) {
-    $date = $_POST["datetime-local"];
+  if (isset($_POST["date"]) && isset($_POST["link"]) && isset($_POST["description"])) {
+    $date = $_POST["date"];
     $link = $_POST["link"];
     $desc = $_POST["description"];
-    $imp = $_POST["importance"];
+    if (isset($_POST["importance"])) {
+      $imp = true;
+    } else {
+      $imp = false;
+    }
 
     $conn = mysqli_connect('localhost', $user, $pw, 'eh');
     mysqli_set_charset($conn, "utf8");
     if (!$conn) {
       die("Connection failed");
-    }else{
+    } else{
       $sql = "INSERT INTO eisenhower(id, user, etime, link, description, importance)
-      VALUES ('', $user, $date, $link, $desc, $imp)";
+      VALUES ('', '$user', '$date', '$link', '$desc', '$imp')";
       if (mysqli_query($conn, $sql)){
-        echo "success";
-      }else {
+        header("Location: ../pages/eisenhower.php");
+      } else {
         echo "fail";
         echo "Error: " . $sql . "<br>" . mysqli_error($conn);
       }
